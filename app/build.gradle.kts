dce8ae4bb5cbb072d4e26f544147d08aa7198fc0
Adalyat Nazirov <aenazirov@gmail.com>
2024-11-09 20:35:36 +0000

    add empty project, add application plugin

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

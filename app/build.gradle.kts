2bfae7955f44484ed1110c83c648ee46bfe8a718
Adalyat Nazirov <aenazirov@gmail.com>
2024-11-11 22:15:29 +0000

    add makefile, show help and version information

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
     application
+    checkstyle
 }
 
 group = "hexlet.code"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 application {
     mainClass = "hexlet.code.App"
+}
+
+checkstyle {
+    toolVersion = "10.20.1"
 }
 
 repositories {
@@ -14,10 +19,21 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.6")
+
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+        )
+    }
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,78 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.concurrent.Callable;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        versionProvider = App.ManifestVersionProvider.class,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Show this help message and exit.")
+    boolean versionInfoRequested;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Print version information and exit.")
+    boolean usageHelpRequested;
+
+    @Override
+    public Integer call() throws Exception {
+        try {
+            Differ.doWork();
+            return 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    /**
+     * {@link CommandLine.IVersionProvider} implementation
+     * that returns version information from the app-x.x.jar file's
+     * {@code /META-INF/MANIFEST.MF} file.
+     */
+    static class ManifestVersionProvider implements CommandLine.IVersionProvider {
+        public String[] getVersion() throws Exception {
+            Enumeration<URL> resources = CommandLine.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+                try {
+                    Manifest manifest = new Manifest(url.openStream());
+                    if (isApplicableManifest(manifest)) {
+                        Attributes attr = manifest.getMainAttributes();
+                        String title = attr.getValue(Attributes.Name.IMPLEMENTATION_TITLE);
+                        String version = attr.getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+                        return new String[]{title + " " + version};
+                    }
+                } catch (IOException ex) {
+                    return new String[]{"Unable to read from " + url + ": " + ex};
+                }
+            }
+            return new String[0];
+        }
+
+        private boolean isApplicableManifest(Manifest manifest) {
+            Attributes attributes = manifest.getMainAttributes();
+            return "gendiff".equals(get(
+                    attributes,
+                    "Implementation-Title")); // Adjust to match your project's name or title
+        }
+
+        private static Object get(Attributes attributes, String key) {
+            return attributes.get(new Attributes.Name(key));
+        }
     }
 }
